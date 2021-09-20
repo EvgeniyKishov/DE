6830573afb1ca19dfeacea3cf7c7cf3c82f02dba
EvgeniyKishov <58272248+EvgeniyKishov@users.noreply.github.com>
2021-09-21 00:57:20 +0400

    Generating children

--- DE/DE.py
+++ DE/DE.py
@@ -1,9 +1,12 @@
-class fun_data:
+class plot_data:
 	def __init__(self):
 		self.min = -5.0
 		self.max = 5.0
 		self.step = 0.05
 		self.lines_num = 20
+		self.p_style = 'ko'
+		self.v_style = 'bv'
+		self.u_style = 'r*'
 
 class point:
 	def __init__(self, x, y):
@@ -17,6 +20,19 @@ def f(X, Y):
         np.exp(0.5*(np.cos(2*np.pi*X)+np.cos(2*np.pi*Y))) + np.exp(1) + 20.0
 	return Z
 
+def plot_pts(min_val, p_star, func, pts, plt, ax, style):
+	for p in pts:
+		f = func(p.x, p.y)
+		if f <= min_val:
+			min_val = f
+			p_star = p
+
+		plt.plot(p.x, p.y, style)
+		text = "{:.2f}".format(f)
+		ax.annotate(text, (p.x, p.y), fontsize=12)
+
+	return min_val, p_star
+
 def plotf(func, param, pts):
 	import matplotlib.pyplot as plt
 	from matplotlib import cm
@@ -24,28 +40,24 @@ def plotf(func, param, pts):
 
 	fig, ax = plt.subplots()
 
-	X = np.arange(param.min, param.max, param.step)
-	Y = np.arange(param.min, param.max, param.step)
+	X = np.arange(param.min-0.5, param.max+0.5, param.step)
+	Y = np.arange(param.min-0.5, param.max+0.5, param.step)
 	X, Y = np.meshgrid(X, Y)
 
 	Z = func(X, Y)
 
 	surf = ax.contour(X, Y, Z, param.lines_num)
-	#ax.clabel(surf, inline=True, fontsize=10)
 	fig.colorbar(surf, shrink=0.5, aspect=10)
 
 	min_val = 1.0e30
 	p_star = point(0, 0)
-	for p in pts:
-		f = func(p.x, p.y)
-		if f <= min_val:
-			min_val = f
-			p_star = p
+	if "p" in pts:
+		min_val, p_star = \
+			plot_pts(min_val, p_star, func, pts["p"], plt, ax, param.p_style)
+	if "v" in pts:
+		min_val, p_star = \
+			plot_pts(min_val, p_star, func, pts["v"], plt, ax, param.v_style)
 
-		plt.plot(p.x, p.y, 'ko')
-		text = "{:.2f}".format(f)
-		ax.annotate(text, (p.x, p.y), fontsize=12)
-	
 	text = "Min. goal func. value from current population = {:.3f}".format(min_val)
 	ax.annotate(text, xy=(0.0,1.1), xycoords='axes fraction', fontsize=12)
 	text = "Best candidate point = ({:.3f},{:.3f})".format(p_star.x, p_star.y)
--- DE/main.py
+++ DE/main.py
@@ -1,9 +1,53 @@
 from DE import *
 import random as rd
 
-param = fun_data()
-pts = []
-for i in range(0, 10):
-    p = point(rd.uniform(-5,5), rd.uniform(-5,5))
-    pts.append(p)
+def print_pts(pts):
+    for p in pts:
+        print("{:.2f}, {:.2f}".format(p.x, p.y))
+
+def compute_rand_ids(pts_num):
+    import random as rd
+    ids = list(range(0, pts_num))
+    r1 = rd.randrange(0, pts_num)
+    del ids[r1]
+    r2_tmp = rd.randrange(0, pts_num-1)
+    r2 = ids[r2_tmp]
+    del ids[r2_tmp]
+    r3_tmp = rd.randrange(0, pts_num-2)
+    r3 = ids[r3_tmp]
+
+    return r1, r2, r3
+
+#initial population
+pop_num = 10
+param = plot_data()
+p = []
+for i in range(0, pop_num):
+    pt = point(rd.uniform(-5,5), rd.uniform(-5,5))
+    p.append(pt)
+
+pts = {"p": p}
 plotf(f, param, pts)
+
+#generating children
+v = []
+F = 0.5
+for p in pts["p"]:
+    r1, r2, r3 = compute_rand_ids(pts_num)
+    v_tmp = point(0.0, 0.0)
+    v_tmp.x = pts["p"][r1].x + F*(pts["p"][r2].x - pts["p"][r3].x)    
+    v_tmp.y = pts["p"][r1].y + F*(pts["p"][r2].y - pts["p"][r3].y)
+    #imposing box constraints
+    v_tmp.x = max(param.min, min(v_tmp.x, param.max))
+    v_tmp.y = max(param.min, min(v_tmp.y, param.max))
+
+    v.append(v_tmp)
+
+pts.update({"v": v})
+plotf(f, param, pts)
+
+
+
+
+
+
